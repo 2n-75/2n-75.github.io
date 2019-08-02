fd1e6975545ac97f3b8ad86963d44c4897f7a3cb
kanako katsumata <may.2s.gp31@gmail.com>
2019-08-02 23:46:04 +0900

    file upload

---
+++ music-work/element/score__template.php
@@ -0,0 +1,53 @@
+<div id="score-component" class="swiper-container">
+	<div class="swiper-wrapper">
+		<score v-for="question in questions" v-bind:key="question.id" v-bind:question="question"></score>
+		<div class="swiper-slide">
+			<div class="swiper-slide__inner text-center">
+				<div class="resultText__wrapper">
+					<p>おつかれさまでした！</p>
+					<p id="resultText">結果</p>
+					<div class="btn-square__wrapper text-center">
+						<a href="./top.php" class="btn-square">もどる</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="prevBtn" class="swiper-button-prev"></div>
+	<div id="nextBtn" class="swiper-button-next"></div>
+</div>
+
+<!-- vue template -->
+<script type="text/x-template" id="score-template">
+	<div class="swiper-slide" >
+	<div class="swiper-slide__inner">
+		<!--出題エリア-->
+		<section id="dropzone" class="dropzone">
+			<div>
+				<p class="middleLine"></p>
+				<p id="timing-bar" class="timing-bar absolute">|</p>
+				<div class="box absolute hint hidden" v-for="item, index in hints" v-bind:key="item.no" v-bind:class="item.className" v-bind:style="{left: item.boxPos + '%' }"></div>
+			</div>
+
+			<div :options="{group:'ITEMS'}" class="dropzone__inner flex-area">
+				<div class="box absolute" v-for="item, index in items" v-bind:key="item.no" v-bind:class="item.className" v-bind:style="{left: item.boxPos + '%' }"></div>
+			</div>
+		</section>
+
+		<!--回答エリア-->
+
+		<section class="relative">
+			<div :options="{group:'ITEMS'}" class="flex-area">
+				<div class="box box--border" v-for="item, index in items2" v-bind:key="item.no" v-bind:class="item.className" v-on:click="noteClick(item.length)"></div>
+			</div>
+			<div class="mg-10 btn-sound-frame">
+				<!--
+					<div class="btn-circle btn-sound absolute" onclick="playSound(2) , barActive()">
+					<span><i class="fas fa-music"></i></span>
+				</div>
+			-->
+			</div>
+		</section>
+		</div>
+	</div>
+</script>
